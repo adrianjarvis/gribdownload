4efd6e746e9fe0578fc38d00a8db044a5da897ed
Adrian Jarvis <adrian.jarvis@clear.net.nz>
2012-10-12 20:05:22 +1300

    First commit

---
+++ src/main/java/jarvis/gribdownloadspring/GfsFileNameFilter.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jarvis.gribdownloadspring;
+
+import java.util.regex.Pattern;
+import org.apache.camel.component.file.GenericFile;
+import org.apache.camel.component.file.GenericFileFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Adge
+ */
+public class GfsFileNameFilter<T> implements GenericFileFilter<T> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericFileFilter.class);
+
+    public GfsFileNameFilter() {
+        LOGGER.info("Created!");
+    }
+
+    
+    
+    @Override
+    public boolean accept(GenericFile<T> file) {
+        boolean result = false;
+        if (file.isDirectory()) {
+            if (Pattern.matches("RD\\.\\d{8}", file.getFileName())) {
+                result = true;
+            } else if (Pattern.matches("RD\\.\\d{8}/PT.grid_DF.gr2", file.getFileName())) {
+                result = true;
+            }
+        } else if (Pattern.matches("fh\\.00\\d\\d_tl\\.press_gr\\.1p0deg", file.getFileNameOnly())) {
+            result = true;
+        }
+        if (result) {
+            LOGGER.info("file: {}, {}", file.getFileName(), file.getFileNameOnly());
+        }
+        return result;
+    }
+    
+}
