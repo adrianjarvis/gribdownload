9bdaa85a7f9913da0906500a9387e5d88a3d0d9f
Adrian Jarvis <adrian.jarvis@clear.net.nz>
2012-10-16 19:57:32 +1300

    Code tidy up, removed unused imports

--- src/main/java/jarvis/gribdownloadspring/AppConfig.java
+++ src/main/java/jarvis/gribdownloadspring/AppConfig.java
@@ -1,12 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package jarvis.gribdownloadspring;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spring.javaconfig.SingleRouteCamelConfiguration;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
