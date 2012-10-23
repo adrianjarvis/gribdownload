946491f5b62dc6a0f75b071be013c1f79a2f9342
Adrian Jarvis <adrian.jarvis@clear.net.nz>
2012-10-23 22:08:45 +1300

    Examine the grib file (currently only checking the edition).

--- src/main/java/jarvis/gribdownloadspring/GfsRouteBuilder.java
+++ src/main/java/jarvis/gribdownloadspring/GfsRouteBuilder.java
@@ -15,8 +15,8 @@ public class GfsRouteBuilder  extends SpringRouteBuilder {
     @Override
     public void configure() throws Exception {
         from("ftp://tgftp.nws.noaa.gov/SL.us008001/ST.opnl/MT.gfs_CY.00/?recursive=true&filter=#gfsfilter")
-                .to("file://target/temp_grib?recursive=true").to("bean:gribProcessing");
-        LOGGER.info("Route defined");
+                .to("file://target/temp_grib?recursive=true");
+        from("file://target/temp_grib?recursive=true").to("bean:gribProcessing").to("file://target/done");
     }
     
 }
--- src/main/java/jarvis/gribdownloadspring/GribProcessorBean.java
+++ src/main/java/jarvis/gribdownloadspring/GribProcessorBean.java
@@ -1,23 +1,43 @@
 package jarvis.gribdownloadspring;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
 import org.apache.camel.Message;
 import org.apache.camel.component.file.GenericFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ucar.unidata.io.RandomAccessFile;
+import ucar.grib.GribChecker;
 
 /**
  *
  * @author Adge
  */
 public class GribProcessorBean {
-    
+
+    public static final String FILE_MODE_READONLY = "r";
     private static final Logger LOGGER = LoggerFactory.getLogger(GribProcessorBean.class);
-    
+
     public void process(Message message) {
-        LOGGER.info("Received message {}:{}", message.getMessageId(), message.getHeaders());
-        LOGGER.info("Body: {}", message.getBody());
         GenericFile file = message.getBody(GenericFile.class);
-        LOGGER.info("File type {}", file.getFile().getClass().getName());
+        final String filePath = file.getAbsoluteFilePath();
+        RandomAccessFile accessFile = null;
+        try {
+            accessFile = new RandomAccessFile(filePath, FILE_MODE_READONLY);
+            int edition = GribChecker.getEdition(accessFile);
+            LOGGER.info("file: {}, edition {}", filePath, edition);
+        } catch (IOException ex) {
+            LOGGER.error("Unable to read file: " + filePath, ex);
+        } finally {
+            if (accessFile != null) {
+                try {
+                accessFile.close();
+                } catch (IOException ex) {
+                    LOGGER.warn("Unable to close file", ex);
+                }
+            }
+        }
     }
-    
 }
